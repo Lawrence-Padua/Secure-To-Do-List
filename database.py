18dd37d35f938b05e9d5c03eade5233e9920fd84
Lawrence-Padua <padua.lawrencejayce@ue.edu.ph>
2025-11-12 23:48:45 +0800

    Update database.py
    Changed database to fix issue with tables

--- database.py
+++ database.py
@@ -2,10 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DB_USER = "sql12806529"
-DB_PASSWORD = "4ziYzNDuJd"
+DB_USER = "sql12807411"
+DB_PASSWORD = "nRKAuLBTgq"
 DB_HOST = "sql12.freesqldatabase.com"
-DB_NAME = "sql12806529"
+DB_NAME = "sql12807411"
 
 SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
 
@@ -13,7 +13,7 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-# Dependency
+#Dependency
 def get_db():
     db = SessionLocal()
     try:
